0facc50c78c883e93fcb723ab372b69b2606ce82
geniusq1981 <geniusq1981@users.noreply.github.com>
2023-03-19 01:18:12 +0000

    Deploying to gh-pages from @ favourite-site/aigc-apply@5afb49bb13e5b32b99577fca1884480b71fbb294 🚀

---
+++ _next/static/MhQUYRrfPI9JE47MpW2KR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/910-4711817a330a77b55374.js","static/chunks/pages/index-3250138ff4904c02c907.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
